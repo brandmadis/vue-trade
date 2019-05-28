c60dbd760360c79035d6dfeaea59f65b8475ec54
unknown <marcos.alois.oliva@gmail.com>
2019-05-27 17:07:47 -0700

    Create new basic layout

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('./views/About.vue')
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: () => import('./views/Profile.vue')
+        },
+        {
+            path: '/team',
+            name: 'team',
+            component: () => import('./views/Team.vue')
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import('./views/Projects.vue')
+        }
+    ]
+})
