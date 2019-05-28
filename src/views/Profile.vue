9b0fbf22e610a29109243a76d5456b57ad00a690
unknown <marcos.alois.oliva@gmail.com>
2019-05-28 11:01:26 -0700

    Update  - Create new basic layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is Profile Home Page</h1>
+    <h1 class="subheading grey--text">This is Profile Home Page</h1>
+
+    <v-container class="my-5">content</v-container>
   </div>
 </template>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is Team Home Page</h1>
+    <h1 class="subheading grey--text">This is Team Home Page</h1>
+
+    <v-container class="my-5">content</v-container>
   </div>
 </template>
